a5f1eabeb17685f6f910e61687f1caa14d831f9b
Sushil Dubey <sushilanildubey@gmail.com>
2021-02-08 02:21:26 +0530

    humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ request({url, json:true }, (error, {body})=>{
        callback('Unable to find the location',undefined)
     }
     else{
-    callback(undefined, body.current.weather_descriptions[0] + ". The current temperature is " + body.current.temperature +". It feels like " + body.current.feelslike)
+    callback(undefined, body.current.weather_descriptions[0] + ". The current temperature is " + body.current.temperature +". It feels like " + body.current.feelslike + ". The current humidity is "+ body.current.humidity +"%")
     }
 })
 
